01084504050a323bdd2673615349e5c3ea3062e0
majmal08 <muhammad.ajmal@ovada.com>
2022-10-06 16:29:49 +0500

    updating for live websockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,23 +12,6 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
 
 import Echo from "laravel-echo"
 
@@ -37,10 +20,11 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: 'anyKey',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
+    wsHost: "websocket.ovadadme.org",
+    wssHost: "websocket.ovadadme.org",
+    wsPort: "",
+    wssPort:"",
+    forceTLS: true,
 });
 
 
