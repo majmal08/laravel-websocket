d0fd96a43b03c5900e600d97afa975cd1baf43bd
majmal08 <muhammad.ajmal@ovada.com>
2022-10-06 17:18:21 +0500

    view updated

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    broadcast(new \App\Events\webSocketDemoEvent('some data'));
+    // broadcast(new \App\Events\webSocketDemoEvent('some data'));
     return view('welcome');
 });
