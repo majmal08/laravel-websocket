e6a986c39799515e73dede412395c9bb8288300d
majmal08 <muhammad.ajmal@ovada.com>
2022-10-11 12:48:26 +0500

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,7 +20,7 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: 'anyKey',
-    wsHost: '18.130.172.177',
+    wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
     disableStats: true,
