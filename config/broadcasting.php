6252fd58225bbe137fe2b746eebb1fac248e1200
majmal08 <muhammad.ajmal@ovada.com>
2022-10-11 12:49:06 +0500

    update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '18.130.172.177',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http'
             ],
