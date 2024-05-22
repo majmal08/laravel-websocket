1b1c859c1ecdecc9243f9a8e60dd9ac9778a71cf
Ajmal <muhammad.ajmal@ovada.com>
2024-05-22 17:30:29 +0500

    update function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index_case(Request $request)
+    public function index_case()
     {
        $var_case = "hello world";
        return $var_case;
