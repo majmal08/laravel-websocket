48b623bf33127dadcde6319360f0b28b13ea585a
Ajmal <muhammad.ajmal@ovada.com>
2024-05-23 11:30:38 +0500

    sonarqube

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,90 @@ class UserController extends Controller
     public function index_case()
     {
        $var_case = "hello world";
-       $var = 'test';
+       $var = 'test'; //this is the testing comment
+
+       $data = '<ruleset name= "Laravel PHPCS Rules">
+
+    <description>PHPCS ruleset for Example app.</description>
+
+    <file>app</file>
+
+    <!-- Show progress of the run -->
+    <arg value= "p"/>
+
+    <!-- Show sniff codes in all reports -->
+    <arg value= "s"/>
+
+    <!-- Our base rule: set to PSR12 -->
+    <!-- <rule ref="PSR12">
+        <exclude name="PSR12.Operators.OperatorSpacing.NoSpaceBefore"/>
+        <exclude name="PSR12.Operators.OperatorSpacing.NoSpaceAfter"/>
+    </rule> -->
+
+    <!-- <rule ref= "Generic.Files.LineLength">
+        <properties>
+            <property name="lineLimit" value="80"/>
+            <property name="absoluteLineLimit" value="120"/>
+        </properties>
+    </rule> -->
+    <rule ref="PSR1.Methods.CamelCapsMethodName.NotCamelCaps">
+        <exclude-pattern>tests/</exclude-pattern>
+        <ruleset name= "Laravel PHPCS Rules">
+
+    <description>PHPCS ruleset for Example app.</description>
+
+    <file>app</file>
+
+    <!-- Show progress of the run -->
+    <arg value= "p"/>
+
+    <!-- Show sniff codes in all reports -->
+    <arg value= "s"/>
+
+    <!-- Our base rule: set to PSR12 -->
+    <!-- <rule ref="PSR12">
+        <exclude name="PSR12.Operators.OperatorSpacing.NoSpaceBefore"/>
+        <exclude name="PSR12.Operators.OperatorSpacing.NoSpaceAfter"/>
+    </rule> -->
+
+    <!-- <rule ref= "Generic.Files.LineLength">
+        <properties>
+            <property name="lineLimit" value="80"/>
+            <property name="absoluteLineLimit" value="120"/>
+        </properties>
+    </rule> -->
+    <rule ref="PSR1.Methods.CamelCapsMethodName.NotCamelCaps">
+        <exclude-pattern>tests/</exclude-pattern>
+    </rule>
+    <ruleset name= "Laravel PHPCS Rules">
+
+    <description>PHPCS ruleset for Example app.</description>
+
+    <file>app</file>
+
+    <!-- Show progress of the run -->
+    <arg value= "p"/>
+
+    <!-- Show sniff codes in all reports -->
+    <arg value= "s"/>
+
+    <!-- Our base rule: set to PSR12 -->
+    <!-- <rule ref="PSR12">
+        <exclude name="PSR12.Operators.OperatorSpacing.NoSpaceBefore"/>
+        <exclude name="PSR12.Operators.OperatorSpacing.NoSpaceAfter"/>
+    </rule> -->
+
+    <!-- <rule ref= "Generic.Files.LineLength">
+        <properties>
+            <property name="lineLimit" value="80"/>
+            <property name="absoluteLineLimit" value="120"/>
+        </properties>
+    </rule> -->
+    <rule ref="PSR1.Methods.CamelCapsMethodName.NotCamelCaps">
+        <exclude-pattern>tests/</exclude-pattern>
+    </rule>
+    </rule>';
+
        return $var_case;
 
     }
