356a8362020d04c725ac148587071b29657480e4
Ajmal <muhammad.ajmal@ovada.com>
2024-06-26 17:07:20 +0500

    add unused variable

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BaseController extends Controller
+{
+    public  function successResponse($status , $data=[], $message, $code)
+    {
+        return response([
+            'status' => $status,
+            'data' => $data,
+            'message' => $message
+        ], $code);
+    }
+
+    public function errorResponse($status, $message, $code)
+    {
+        $unused_var = "un used variable";
+        return response([
+            'status' => $status,
+            'message' => $message
+        ], $code);
+    }
+}
