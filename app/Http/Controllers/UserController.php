d242754a5af258a153ba8fd561dc46bceea04c2f
Ajmal <muhammad.ajmal@ovada.com>
2024-06-26 19:07:00 +0500

    Fix: all the issue in user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,10 @@ use App\Http\Controllers\BaseController;
 class UserController extends BaseController
 {
 
-    private $snake_case;
-
     public function camel_omething()
     {
 
        $var_case = "hello world";
-       $var_case = "hello world";
-       $new = "unused var";
 
        return $var_case;
 
