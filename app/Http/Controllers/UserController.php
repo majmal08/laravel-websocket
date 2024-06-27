43eb238557e251080f4f0ccdce54071309e30f35
Ajmal <muhammad.ajmal@ovada.com>
2024-06-27 16:15:33 +0500

    one issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,8 @@ class UserController extends BaseController
 
     public function camel_omething()
     {
-
        $var_case = "hello world";
        $var = "Not using";
-
        return $var_case;
 
     }
